893aec50e6f6114e3105ee3eb7678cb928d360ca
fajrul <fajrul452@gmail.com>
2022-07-19 13:48:14 +0700

    error app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,21 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Movie</title>
-
+    <title>SoftCRM</title>
     <link rel="stylesheet" href="dist/app.css" />
-    <link rel="stylesheet" href="assets/css/style.css" />
-
-	<!-- Jquery Javascript Libary Plugin -->
-	<script src="assets/plugins/jquery/jquery-1.11.3.js"></script>
-
-	<!-- Owl Cerousel Slider Plugin -->
-	<link rel="stylesheet" href="assets/plugins/owlcarousel/owl.carousel.css">
-	<link rel="stylesheet" href="assets/plugins/owlcarousel/owl.theme.css">
-	<script src="assets/plugins/owlcarousel/owl.carousel.js"></script>
-
-	<!-- System Cumtop Js -->
-	<script src="assets/js/custom.js"></script>
+    <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
     <div id="app"></div>
